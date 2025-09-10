8b1cdbf4b386fd97de8c07ed204db9d752fdf2f3
famous <138824251+aashiruu@users.noreply.github.com>
2025-09-10 21:15:47 +0100

    Create providers.tf

---
+++ vm-deployment/terraform/providers.tf
@@ -0,0 +1,16 @@
+
+```hcl
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-east-1"  # Change to your preferred region
+}
